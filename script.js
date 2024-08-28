11b597387ddeb8f74bfaf7e46d10c67a456a5a38
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-28 15:18:46 +0530

    Create a File Using Node js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// let a = 23;
-// let b = 234;
-// const c = 45;
-
-// module.exports = {a,b};
-
-// function add(a,b) {
-//     return a +b;
-// }
-
-// console.log(add(4343,34343));
 
 //+++++++++++++++++++++ Fie Systems on node JS++++++++++++++++
+// Write file, read file,update file,append file,delete file,forder creation, copy,rename,move
 
+// 1. Create a File 
+
+const fileStstem = require('fs');
+
+fileStstem.writeFile("Node,.txt","Learn Node js" , function(err){
+    if(err) console.log(err);
+    else console.log("File created successfully")
+})
