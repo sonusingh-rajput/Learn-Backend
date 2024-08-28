c0f42c4d5186fad1eaa153d8206892e652eafe61
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-29 05:06:50 +0530

    Starting a server Using Nodemon

--- App.js
+++ App.js
@@ -7,3 +7,17 @@
 // })
 
 // server.listen(3000);
+
+// ++++++++++++++++++ (npm i nodemon --save-dev) Creating a Server Using Nodemon (npx nodeman file name start Server) +++++++++++++++++
+
+// const http = require("node:http");
+
+// let server = http.createServer((req,res) => {
+// res.end("welcome to nodemon!");
+// })
+
+// server.listen(3000);
+
+
+// ++++++++++++++++++ HTTP Routings +++++++++++++++++
+
