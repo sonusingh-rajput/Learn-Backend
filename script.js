910b32063e1cf7c36dc758966b33aceef035763f
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-28 15:08:20 +0530

    Learn Node File System

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let a = 23;
-let b = 234;
-const c = 45;
+// let a = 23;
+// let b = 234;
+// const c = 45;
 
-module.exports = {a,b};
+// module.exports = {a,b};
+
+// function add(a,b) {
+//     return a +b;
+// }
+
+// console.log(add(4343,34343));
+
+//+++++++++++++++++++++ Fie Systems on node JS++++++++++++++++
+
--- script1.js
+++ script1.js
@@ -1,3 +1,3 @@
- var data  = require('./script')
+//  var data  = require('./script')
 
-console.log(data)
+// console.log(data)
